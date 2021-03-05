410be85bfbba4f74ac249d51a5dbbb209267177b
Sasi Reddyvari <48015531+sasireddyvari@users.noreply.github.com>
2021-03-05 19:17:12 +0530

    Add files via upload

---
+++ speechToText.py
@@ -0,0 +1,34 @@
+import speech_recognition as sr
+
+
+def speech2Text(file):
+    AUDIO_FILE = file
+    r = sr.Recognizer()
+
+    with sr.AudioFile(AUDIO_FILE) as source:
+        audio = r.record(source)
+
+    try:
+        textdata = r.recognize_google(audio)
+        print("Text data: " + textdata)
+        return textdata
+
+    except sr.UnknownValueError:
+        print(" Audio Error")
+        textdata="Audio Error"
+        return textdata
+
+    except sr.RequestError as e:
+        print("Could not request results from Google API; {0}".format(e))
+        textdata = "Audio Error"
+        return textdata
+    else:
+        textdata = "Audio Error"
+        return textdata
+
+
+#Write results to a txt file
+#file = open("result.txt","w")
+#file.write(textdata)
+#file.writelines(L) 
+#file.close()
